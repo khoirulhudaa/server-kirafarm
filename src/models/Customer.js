8deba8f65fb3657fca7a7e00f1006051a0de107d
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-03-27 22:14:00 +0700

    fix

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const Customer = sequelize.define('Customer', {
-    id: { type: DataTypes.STRING, primaryKey: true, allowNull: false },
+    id: {   type: DataTypes.STRING(36), primaryKey: true, allowNull: false },
     name: { type: DataTypes.STRING, allowNull: false },
     phone: { type: DataTypes.STRING, unique: true, allowNull: false },
     email: DataTypes.STRING,
