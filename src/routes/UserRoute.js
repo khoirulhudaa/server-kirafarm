69dc94abecc95e71ae45c20b389fcc6c603ac284
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-03-28 15:32:58 +0700

    fix

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -12,19 +12,19 @@ const AuthMiddleware = require('../middlewares/AuthMiddleware');
 const router = express.Router();
 
 // Hanya OWNER & ADMIN yang bisa lihat semua user
-router.get('/', AuthMiddleware(['OWNER', 'ADMIN']), getAll);
-router.get('/:id', AuthMiddleware(['OWNER', 'ADMIN']), getById);
+router.get('/', AuthMiddleware(['OWNER', 'ADMIN', 'SELLER']), getAll);
+router.get('/:id', AuthMiddleware(['OWNER', 'ADMIN', 'SELLER']), getById);
 
 // Hanya OWNER & ADMIN yang bisa buat user baru
-router.post('/', AuthMiddleware(['OWNER', 'ADMIN']), create);
+router.post('/', AuthMiddleware(['OWNER', 'ADMIN', 'SELLER']), create);
 
 // Update profile (user bisa update dirinya sendiri, atau ADMIN/OWNER update orang lain)
-router.put('/:id/profile', AuthMiddleware(['OWNER', 'ADMIN']), updateProfile);
+router.put('/:id/profile', AuthMiddleware(['OWNER', 'ADMIN', 'SELLER']), updateProfile);
 
 // Update password (user update sendiri atau ADMIN/OWNER)
-router.patch('/:id/password', AuthMiddleware(['OWNER', 'ADMIN']), updatePassword);
+router.patch('/:id/password', AuthMiddleware(['OWNER', 'ADMIN', 'SELLER']), updatePassword);
 
 // Soft delete / activate (hanya OWNER & ADMIN)
-router.patch('/:id/status', AuthMiddleware(['OWNER', 'ADMIN']), softDelete);
+router.patch('/:id/status', AuthMiddleware(['OWNER', 'ADMIN', 'SELLER']), softDelete);
 
 module.exports = router;
